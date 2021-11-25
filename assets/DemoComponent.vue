f94f81d3d9d4a2ee6dd2a2e26f9260cef0abe3d5
drkilla <sa.stanojevicaleksandar@gmail.com>
2021-11-26 00:02:14 +0100

    création template vueJs in symfony

---
+++ assets/DemoComponent.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="container">
+        <h1>Ceci est notre composant de Démo !! </h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "DemoComponent"
+
+};
+</script>
---
+++ assets/app.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import DemoComponent from "./DemoComponent";
+
+
+
+new Vue({
+    components: {DemoComponent},
+    template: "<demoComponent />"
+
+}).$mount("#demoComponent");
+
